04a867ea49fffd82cd1baffbc0eb2a4f37d2003e
黄轩 <huangxuan@fangdd.com>
2016-03-30 09:47:49 +0800

    Initial Commit

---
+++ HXMusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXMusicPlayer
+//
+//  Created by 黄轩 on 16/3/30.
+//  Copyright © 2016年 黄轩 blog.libuqing.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXMusicPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HXMusicPlayer
+//
+//  Created by 黄轩 on 16/3/30.
+//  Copyright © 2016年 黄轩 blog.libuqing.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
